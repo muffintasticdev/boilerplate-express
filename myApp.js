0c1f3013f92a068491290d75228dd08d3712ee24
Manuel Tabilo <tabilocallejas@gmail.com>
2022-07-27 14:22:00 -0400

    html view to root path

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
 console.log("Hello World")
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+let absoultePathHtml = __dirname+'/views/index.html';
+app.get("/", (req, res) => res.sendFile(absoultePathHtml) );
 
 
 
